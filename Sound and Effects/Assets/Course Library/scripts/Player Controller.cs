455472708914597b53b7e5747a21b13713358d7b
Jaydennboi <Jaydenmarques098@riversideunified.org>
2023-10-12 11:34:21 -0700

    update 2

--- Sound and Effects/Assets/Course Library/scripts/Player Controller.cs
+++ Sound and Effects/Assets/Course Library/scripts/Player Controller.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
         { 
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
     }
 
